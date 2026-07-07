3b757b08e9e68443cecbb57f8f5f589e728b7998
Shaurya <edshaurya@gmail.com>
2026-07-07 14:46:27 +0530

    implement blog deletion after confirmation

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,6 +1,26 @@
 import { Link } from "react-router-dom";
+import api from "../services/api";
+import { useNavigate } from "react-router-dom";
 
 const BlogCard = ({ blog }) => {
+  const navigate = useNavigate();
+  const handleDelete = async (id) => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this blog?",
+    );
+
+    if (!confirmDelete) return;
+
+    try {
+      await api.delete(`/blogs/${id}`);
+
+      alert("Blog deleted successfully!");
+
+      navigate(0);
+    } catch (error) {
+      alert(error.response?.data?.message || "Delete failed");
+    }
+  };
   return (
     <div className="bg-slate-900 rounded-xl border border-slate-800 p-5 hover:border-purple-500 transition">
       <h2 className="text-2xl font-semibold text-white mb-3">{blog.title}</h2>
@@ -26,6 +46,12 @@ const BlogCard = ({ blog }) => {
         >
           Edit
         </Link>
+        <button
+          onClick={() => handleDelete(blog._id)}
+          className="text-red-400 hover:text-red-300"
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
